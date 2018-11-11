0364b68f05145553b4cd46e8e8bfd56475ebf769
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-11-11 17:21:43 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,8 +4,12 @@ def my_all?(collection)
   i = 0
   block_return_values = []
   while i < collection.length
-    block_return_values << yield(collection[i])
+    yielded_value = yield(collection[i])
+    block_return_values << yielded_value
+    if !yielded_value
+      return false
+    end
     i += 1
   end
-  
+  true
 end
